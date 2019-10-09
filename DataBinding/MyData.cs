c641ea768915bcb8303bcceb02127904c43cdd76
Constant Dupuis <dev-tools@alterface.com>
2019-10-09 17:44:40 +0200

    Create a MyKnob user control :-)
    Good start !

---
+++ DataBinding/MyData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace DataBinding
+{
+	public class DataItem
+	{
+		private int id;
+		private string path;
+		private float value;
+
+		public DataItem()
+		{
+		}
+
+		public DataItem(int id, string path, float value)
+		{
+			this.id = id;
+			this.path = path;
+			this.value = value;
+		}
+
+		public int Id { get => id; set => id = value; }
+		public string Path { get => path; set => path = value; }
+		public float Value { get => value; set => this.value = value; }
+
+		public override string ToString()
+		{
+			return "" + id + " " + path + " " + value ;
+		}
+	}
+
+	public class MyData
+	{
+		public MyData()
+		{
+			_collection.Add(new DataItem(0, "/", 0.1f));
+			_collection.Add(new DataItem(1, "/one/", 0.2f));
+			_collection.Add(new DataItem(2, "/two/", 0.3f));
+		}
+
+		private string _colorName = "Red";
+		private ObservableCollection<DataItem> _collection = new ObservableCollection<DataItem>();
+		
+
+		public string ColorName { get => _colorName; set => _colorName = value; }
+		public ObservableCollection<DataItem> MyCollection { get => _collection; set => _collection = value; }
+	}
+}
---
+++ Knob/MyKnob01.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Knob
+{
+	/// <summary>
+	/// Interaction logic for MyKnob01.xaml
+	/// </summary>
+	public partial class MyKnob01 : UserControl, INotifyPropertyChanged
+	{
+		public MyKnob01()
+		{
+			InitializeComponent();
+			DataContext = this;
+			//Console.WriteLine("MyKnob01::MyKnob01");
+		}
+
+		private double _angle = 0.0f;
+		private bool dialing = false;
+		private Point startDial;
+
+		private const double minAngle = -145.0;
+		private const double maxAngle = 145.0;
+
+		public double Angle { 
+			get => _angle;
+			set
+			{
+				if (_angle != value)
+				{
+					_angle = value;
+					OnPropertyChanged("Angle");
+				}
+				
+			}
+		}
+		
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void setRange(double min, double max)
+		{ 
+
+		}
+
+		protected void OnPropertyChanged(string name)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+
+		private void KnobGrid_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			//Console.WriteLine("MyKnob01::KnobGrid_MouseDown");
+			dialing = true;
+			startDial = e.GetPosition(this);
+			Mouse.Capture(sender as IInputElement);
+		}
+
+		private void KnobGrid_MouseUp(object sender, MouseButtonEventArgs e)
+		{
+			//Console.WriteLine("MyKnob01::KnobGrid_MouseUp");
+			dialing = false;
+			Mouse.Capture(null);
+		}
+
+		private void KnobGrid_MouseMove(object sender, MouseEventArgs e)
+		{
+			//Console.WriteLine("MyKnob01::KnobGrid_MouseDown");
+			if (dialing)
+			{
+				double diff = e.GetPosition(this).X - startDial.X;
+				//Console.WriteLine("MyKnob01::KnobGrid_MouseDown - Diff " + diff);
+
+				double newAngle = _angle + diff;
+				if (newAngle < minAngle)
+				{
+					newAngle = minAngle;
+				}
+				else if (newAngle > maxAngle)
+				{
+					newAngle = maxAngle;
+				}
+				Angle = newAngle;
+
+				startDial = e.GetPosition(this);
+			}
+		}
+	}
+}
